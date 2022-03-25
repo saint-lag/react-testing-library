8669e250d77e12b128dd99824d4e2d9500c962d1
saint-lag <maiasena.gabriel@gmail.com>
2022-03-25 15:56:40 -0300

    Primeiro Commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    it('O primeiro link deve possuir o texto Home', () => {
+      renderWithRouter(<App />);
+    });
+    it('O segundo link deve possuir o texto About', () => {});
+    it('O terceiro link deve possuir o texto Favorite Pokémons', () => {});
+  });
