4fb85304b998e0928bcb4c7fb5b58ca38cb1230f
Gabriel Maia <maiasena.gabriel@gmail.com>
2022-03-28 17:48:35 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -7,7 +7,52 @@ describe('Teste se o topo da aplicação contém um conjunto fixo de links de na
   () => {
     it('O primeiro link deve possuir o texto Home', () => {
       renderWithRouter(<App />);
+      const homeLink = screen.getByRole('link', { name: /home/i });
+      expect(homeLink).toBeInTheDocument();
     });
-    it('O segundo link deve possuir o texto About', () => {});
-    it('O terceiro link deve possuir o texto Favorite Pokémons', () => {});
+    it('O segundo link deve possuir o texto About', () => {
+      renderWithRouter(<App/>);
+      const aboutLink = screen.getByRole('link', {name: /about/i});
+      expect(aboutLink).toBeInTheDocument();
+    });
+    it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+      renderWithRouter(<App/>);
+      const favoritePokemonsLink = screen.getByRole('link', {name: /favorite pokémons/i});
+      expect(favoritePokemonsLink).toBeInTheDocument();
+    });
   });
+
+describe('Testes de redirecionamento dos Links',
+  () => {
+    it('URL "/" => Home',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const homeLink = screen.getByRole('link', 
+      { name: /home/i });
+      userEvent.click(homeLink);
+      expect(history.location.pathname).toBe('/');
+    });
+    it('URL "/about" => About',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const aboutLink = screen.getByRole('link', 
+      { name: /about/i });
+      userEvent.click(aboutLink);
+      expect(history.location.pathname).toBe('/about');
+    });
+    it('URL "/favorites" => Pokémons Favoritados',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const favoritePokemonsLink = screen.getByRole('link', 
+      { name: /favorite pokémons/i });
+      userEvent.click(favoritePokemonsLink);
+      expect(history.location.pathname).toBe('/favorites');
+    });
+    it('URL desconhecida => NOT FOUND',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/not-a-link');
+      expect(history.location.pathname).toBe('/notfound');
+    });
+  }
+)
