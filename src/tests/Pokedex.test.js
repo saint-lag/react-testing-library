7e747c3e138c72093b5cc96d495f44750ca925e4
Gabriel Maia <maiasena.gabriel@gmail.com>
2022-03-30 00:43:50 -0300

    [Testado Localmente] Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -19,11 +19,10 @@ describe('Testando a Pokédex', () => {
     const nextPokemonBtn = screen.getByRole('button', { name: /próximo pokémon/i });
     for(let i = 0; i <= pokemons.length; i++) {
       const pokemonNamesTester = screen.getAllByTestId('pokemon-name');
-      const previousPokemonName = screen.getByTestId('pokemon-name');
+      const previousPokemonName = Object.values(screen.getByTestId('pokemon-name'))[1].children;
       userEvent.click(nextPokemonBtn);
-      const currentPokemonName = screen.getByTestId('pokemon-name');
+      const currentPokemonName = Object.values(screen.getByTestId('pokemon-name'))[1].children;
       expect(pokemonNamesTester).toHaveLength(1);
-      console.log(previousPokemonName, currentPokemonName);
       const bool = previousPokemonName !== currentPokemonName;
       expect(bool).toBeTruthy();
   }
@@ -33,6 +32,38 @@ describe('Testando a Pokédex', () => {
     expect(filterByAll).toBeInTheDocument();
     const filters = screen.getAllByTestId('pokemon-type-button');
     expect(filters).toHaveLength(7);
-    // filters.forEach((filter) => console.log(filter.return));
+    const filtersTypes = filters.map((filter) => Object.values(filter)[1].children);
+    const filtersTypesSet = new Set(filtersTypes);
+    expect([...filtersTypesSet].length).toEqual(filtersTypes.length);
+  });
+  it('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo', () => {
+    const filters = screen.getAllByTestId('pokemon-type-button');
+    const nextPokemonBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    filters.forEach((filter) => {
+      userEvent.click(filter);
+      const type = Object.values(filter)[1].children;
+      const pokemonsOfCurrentType = [];
+      let bool = true;
+      while(bool) {
+        const pokemonName = Object.values(screen.getByTestId('pokemon-name'))[1].children;
+        pokemonsOfCurrentType.includes(pokemonName) ? bool = false : pokemonsOfCurrentType.push(pokemonName);
+        const currentPokemonType = Object.values(screen.getByTestId('pokemon-type'))[1].children;
+        expect(type).toBe(currentPokemonType);
+        userEvent.click(nextPokemonBtn);
+      }
+    })
+  });
+  it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const filterByAll = screen.getByRole('button', { name: /all/i });
+    userEvent.click(filterByAll);
+    const nextPokemonBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    const allTypes = ['Eletric', 'Fire,', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    const allFoundTypes = [];
+    for(let i = 0; i <= 20; i++) {
+      const currentPokemonType = Object.values(screen.getByTestId('pokemon-type'))[1].children;
+      allFoundTypes.push(currentPokemonType);
+      userEvent.click(nextPokemonBtn);
+    }
+    expect(allTypes.length).toEqual([...new Set(allFoundTypes)].length);
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,3 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
