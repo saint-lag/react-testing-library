52f7b1b026a81d3bdea4219e714d36fa7b13d3b0
Gabriel Maia <maiasena.gabriel@gmail.com>
2022-03-30 01:14:57 -0300

    [Estruturando] Iniciando último Requisiito

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App'
+
+describe('Testando Pokemon Details', () => {
+  const POKEMON_PAGE_LOCATION = '/pokemons/4';
+  const POKEMON_NAME = 'Charmander';
+  it('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(POKEMON_PAGE_LOCATION);
+    console.log(POKEMON_NAME);
+    const pokemonDetails = screen.getByText(`${POKEMON_NAME} Details`);
+    expect(pokemonDetails).toBeInTheDocument();
+  });
+})
